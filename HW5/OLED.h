80eb6f61082598227117ce4cf0760c8a2758e042
prapul <prapul.kashyap1@gmail.com>
2015-04-21 11:14:52 -0500

    HW5 first commit

---
+++ HW5/OLED.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   OLED.h
+ * Author: prapul
+ *
+ * Created on 13 April, 2015, 6:55 PM
+ */
+
+void oled_pos_set(int row,int col);
+int oled_row_get();
+int oled_col_get();
+void print_string_oled(char *str);
+void screen_on();
+
